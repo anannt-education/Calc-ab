e35792b94629c994f14bb24e7422624bf3fb2e78
anannt-education <hello.anannt@gmail.com>
2026-09-10 12:20:16 +0530

    Add AP Calculus AB platform source

--- src/lib/content/lessons.ts
+++ src/lib/content/lessons.ts
@@ -1,1 +1,6 @@
-PLACEHOLDER_USE_PYTHON
+import type { LessonVersion } from "../types";
+import { LESSONS_A } from "./lessons-a";
+import { LESSONS_B } from "./lessons-b";
+
+export const LESSONS: LessonVersion[] = [...LESSONS_A, ...LESSONS_B];
+export const LESSON_BY_ID = Object.fromEntries(LESSONS.map((l) => [l.id, l]));
